343aafed5f20160ebe6a3ef1a04678bebdb32225
cahyo-0103 <151585229+cahyo-0103@users.noreply.github.com>
2024-10-25 11:32:49 +0700

    Add files via upload

--- jquery2.js
+++ jquery2.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
             // Redirect ke halaman berikutnya setelah login berhasil
             setTimeout(function() {
-                window.location.href = "index copy.html"; // Ganti 'dashboard.html' dengan halaman tujuan
+                window.location.href = "selamat.html"; // Ganti 'dashboard.html' dengan halaman tujuan
             }, 2000); // 2 detik delay sebelum pindah halaman
         } else {
             $('#loginMessage1').text("Invalid username or password");
